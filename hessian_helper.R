19020a0c0b59ec94d4632b7d3a1ff672bfa72258
bbbales2 <bbbales2@gmail.com>
2018-07-04 13:10:09 -0700

    Added a few bits and pieces to hessian_helper so it'll work with outside projects

--- hessian_helper.R
+++ hessian_helper.R
@@ -3,13 +3,17 @@ library(Rcpp)
 
 load_model = function(model_file, params_list) {
   envir = list2env(params_list)
-  stan_rdump(names(params_list), file = "data.dump", envir = envir)
+  if(length(params_list) == 0) {
+    system("touch data.dump")
+  } else {
+    stan_rdump(names(params_list), file = "data.dump", envir = envir)
+  }
   
   if(system(paste0('/home/bbales2/cmdstan/bin/stanc --allow_undefined --name=linear_regression_model ', model_file)) != 0) {
     stop("model '", model_file,"' does not exist")
   }
   system('mv linear_regression_model.cpp linear_regression_model.hpp')
-  
+  system(paste0('cp ', paste0(getSrcDirectory(function(dummy) {dummy})), '/helper.cpp helper.cpp'))
   sourceCpp("helper.cpp")
   set_data("data.dump")
 }
